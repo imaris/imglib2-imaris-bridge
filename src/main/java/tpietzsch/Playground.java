e377b813d2c2b610ddb8f47bad2a15b563cc8566
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-03-19 22:10:12 +0100

    CachedCellImg proof-of-concept backed by ImarisLib

--- src/main/java/tpietzsch/Playground.java
+++ src/main/java/tpietzsch/Playground.java
@@ -6,9 +6,18 @@ import Imaris.IApplicationPrx;
 import Imaris.IDataSetPrx;
 import Imaris.tType;
 import ImarisServer.IServerPrx;
+import bdv.util.BdvFunctions;
+import bdv.util.BdvOptions;
+import bdv.util.volatiles.VolatileViews;
 import com.bitplane.xt.BPImarisLib;
+import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.cache.img.CellLoader;
+import net.imglib2.cache.img.ReadOnlyCachedCellImgFactory;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 
-import static Imaris.IApplicationPrxHelper.*;
+import static Imaris.IApplicationPrxHelper.checkedCast;
+import static net.imglib2.cache.img.ReadOnlyCachedCellImgOptions.options;
 
 public class Playground
 {
@@ -37,6 +46,62 @@ public class Playground
 		lib.Disconnect();
 	}
 
+	static class ImarisDataset
+	{
+		private final IDataSetPrx dataset;
+
+		private final long[] xyzDims;
+
+		private final double[] calib;
+
+		public ImarisDataset( final IDataSetPrx dataset ) throws Error
+		{
+			this.dataset = dataset;
+			final int sx = dataset.GetSizeX();
+			final int sy = dataset.GetSizeY();
+			final int sz = dataset.GetSizeZ();
+
+			final double maxX = dataset.GetExtendMaxX();
+			final double minX = dataset.GetExtendMinX();
+			final double maxY = dataset.GetExtendMaxY();
+			final double minY = dataset.GetExtendMinY();
+			final double maxZ = dataset.GetExtendMaxZ();
+			final double minZ = dataset.GetExtendMinZ();
+			calib = new double[] {
+					( maxX - minX ) / sx,
+					( maxY - minY ) / sy,
+					( maxZ - minZ ) / sz
+			};
+
+			xyzDims = new long[] { sx, sy, sz };
+		}
+
+		final Img< UnsignedByteType > getImage( final int channel, final int timepoint )
+		{
+			final ReadOnlyCachedCellImgFactory factory = new ReadOnlyCachedCellImgFactory();
+			final CellLoader< UnsignedByteType > loader = cell -> {
+				final byte[] out = ( byte[] ) cell.getStorageArray();
+				final byte[] in = dataset.GetDataSubVolumeAs1DArrayBytes(
+						( int ) cell.min( 0 ),
+						( int ) cell.min( 1 ),
+						( int ) cell.min( 2 ),
+						channel,
+						timepoint,
+						( int ) cell.dimension( 0 ),
+						( int ) cell.dimension( 1 ),
+						( int ) cell.dimension( 2 )
+				);
+				System.arraycopy( in, 0, out, 0, out.length );
+			};
+			final CachedCellImg< UnsignedByteType, ? > img = factory.create(
+					xyzDims,
+					new UnsignedByteType(),
+					loader,
+					options().cellDimensions( 16 ) );
+			return img;
+		}
+	}
+
 	public static void main( String[] args ) throws Error
 	{
 		final Playground pg = new Playground();
@@ -52,8 +117,23 @@ public class Playground
 		final tType tType = dataset.GetType();
 		System.out.println( "type = " + tType );
 
-		dataset.GetDataVolumeAs1DArrayBytes(  )
+//		final byte[] bytes = dataset.GetDataVolumeAs1DArrayBytes( 0, 0 );
+//		final long[] dims = { dataset.GetSizeX(), dataset.GetSizeY(), dataset.GetSizeZ() };
+//		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( bytes, dims );
 
+		final ImarisDataset ds = new ImarisDataset( dataset );
+		final Img< UnsignedByteType > img = ds.getImage( 0, 0 );
+		BdvFunctions.show( VolatileViews.wrapAsVolatile( img ), "imaris", BdvOptions.options().sourceTransform( ds.calib ) );
+
+		try
+		{
+			Thread.sleep( 10000 );
+		}
+		catch ( InterruptedException e )
+		{
+			e.printStackTrace();
+		}
+		System.out.println( "shutting down" );
 		pg.disconnect();
 	}
 }
