e2d7ba8089decba104adc34175a94e9d11d60533
tpietzsch <tobias.pietzsch@gmail.com>
2020-11-21 23:56:27 +0100

    Compute all cells and persist to Imaris, single-threaded

--- src/main/java/tpietzsch/ExampleCellLoader.java
+++ src/main/java/tpietzsch/ExampleCellLoader.java
@@ -1,6 +1,7 @@
 package tpietzsch;
 
 import Imaris.Error;
+import Imaris.IDataSetPrx;
 import bdv.util.BdvFunctions;
 import bdv.util.volatiles.VolatileViews;
 import com.bitplane.xt.ImarisCachedCellImg;
@@ -9,12 +10,17 @@ import com.bitplane.xt.ImarisCachedCellImgOptions;
 import com.bitplane.xt.ImarisService;
 import ij.IJ;
 import ij.ImagePlus;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.cache.Cache;
 import net.imglib2.cache.img.CellLoader;
 import net.imglib2.cache.img.SingleCellArrayImg;
+import net.imglib2.img.cell.Cell;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.util.Intervals;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
 import org.scijava.Context;
@@ -35,7 +41,6 @@ public class ExampleCellLoader
 			@Override
 			public void load( final SingleCellArrayImg< UnsignedByteType, ? > cell ) throws Exception
 			{
-				System.out.println( "cell = " + cell );
 				final Cursor< UnsignedByteType > out = cell.cursor();
 				final Cursor< UnsignedByteType > in = Views.interval( img, cell ).cursor();
 				while ( out.hasNext() )
@@ -54,5 +59,29 @@ public class ExampleCellLoader
 		final ImarisCachedCellImg< UnsignedByteType, ? > imarisImg = factory.create( img, loader );
 
 		BdvFunctions.show( VolatileViews.wrapAsVolatile( imarisImg ), "imarisImg" );
+
+		final Cache< Long, ? extends Cell< ? > > cache = imarisImg.getCache();
+		final long numCells = Intervals.numElements( imarisImg.getCellGrid().getGridDimensions() );
+		for ( long i = 0; i < numCells; i++ )
+		{
+			final Long key = i;
+			final Callable< Void > c = () -> {
+				cache.get( key );
+				cache.persist( key );
+				return null;
+			};
+			try
+			{
+				cache.get( i );
+				cache.persist( i );
+			}
+			catch ( ExecutionException e )
+			{
+				e.printStackTrace();
+			}
+		}
+
+		final IDataSetPrx dataset = imarisImg.getDataSet();
+		imaris.app().SetImage( 0, dataset );
 	}
 }
