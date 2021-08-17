330887003462910b9659e6fc17d466583db1fa5d
tpietzsch <tobias.pietzsch@gmail.com>
2021-08-17 13:57:44 +0200

    License headers, javadoc author, and minor cleanups

--- src/main/java/com/bitplane/xt/util/AxisOrderUtils.java
+++ src/main/java/com/bitplane/xt/util/AxisOrderUtils.java
@@ -1,6 +1,33 @@
+/*-
+ * #%L
+ * Expose the Imaris XT interface as an ImageJ2 service backed by ImgLib2.
+ * %%
+ * Copyright (C) 2019 - 2021 Bitplane AG
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 package com.bitplane.xt.util;
 
-import Imaris.tType;
 import bdv.util.AxisOrder;
 import mpicbg.spim.data.sequence.FinalVoxelDimensions;
 import mpicbg.spim.data.sequence.VoxelDimensions;
@@ -9,16 +36,12 @@ import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
 import net.imagej.axis.LinearAxis;
 import net.imagej.space.CalibratedSpace;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
-import net.imglib2.type.numeric.integer.UnsignedShortType;
-import net.imglib2.type.numeric.real.FloatType;
 
-import static Imaris.tType.eTypeFloat;
-import static Imaris.tType.eTypeUInt16;
-import static Imaris.tType.eTypeUInt8;
-
+/**
+ * Get {@code AxisOrder} corresponding to {@code CalibratedSpace}.
+ *
+ * @author Tobias Pietzsch
+ */
 public final class AxisOrderUtils
 {
 	/**
--- src/main/java/com/bitplane/xt/util/CellGridUtils.java
+++ src/main/java/com/bitplane/xt/util/CellGridUtils.java
@@ -1,3 +1,31 @@
+/*-
+ * #%L
+ * Expose the Imaris XT interface as an ImageJ2 service backed by ImgLib2.
+ * %%
+ * Copyright (C) 2019 - 2021 Bitplane AG
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 package com.bitplane.xt.util;
 
 import Imaris.IDataSetPrx;
@@ -5,6 +33,12 @@ import net.imglib2.img.cell.CellGrid;
 import net.imglib2.util.Fraction;
 import net.imglib2.util.Intervals;
 
+/**
+ * Create appropriate {@code CellGrid} for wrapping {@code IDataSetPrx} into
+ * {@code CachedCellImg}.
+ *
+ * @author Tobias Pietzsch
+ */
 public final class CellGridUtils
 {
 	/**
