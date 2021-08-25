532ab1e6f7d5ff2ca4779d62f86a1c8c64e86040
tpietzsch <tobias.pietzsch@gmail.com>
2021-08-25 12:02:58 +0200

    Move Loader/Remover classes to util package

--- src/main/java/com/bitplane/xt/img/ImarisCachedCellImgFactory.java
+++ src/main/java/com/bitplane/xt/img/ImarisCachedCellImgFactory.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -32,8 +32,8 @@ import Imaris.Error;
 import Imaris.IDataSetPrx;
 import com.bitplane.xt.DatasetDimensions;
 import com.bitplane.xt.ImarisApplication;
-import com.bitplane.xt.ImarisDirtyLoaderRemover;
-import com.bitplane.xt.ImarisLoaderRemover;
+import com.bitplane.xt.util.ImarisDirtyLoaderRemover;
+import com.bitplane.xt.util.ImarisLoaderRemover;
 import com.bitplane.xt.util.CellGridUtils;
 import com.bitplane.xt.util.ImarisUtils;
 import com.bitplane.xt.util.MapDimensions;
